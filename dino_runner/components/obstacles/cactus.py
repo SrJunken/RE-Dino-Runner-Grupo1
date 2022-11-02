e6178462b51c99a458447e38ca7b96973adaa3d6
SrJunken <115379329+SrJunken@users.noreply.github.com>
2022-11-02 18:59:44 -0400

    Update cactus.py

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,16 +1,19 @@
 import random
+
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 from .obstacle import Obstacle
 
-
+SMALL_CACTUS_HEIGHT = 71
+LARGE_CACTUS_HEIGHT = 95
 class Cactus(Obstacle):
     def __init__(self, images):
         type = random.randint(0,5)
 
         super().__init__(images, type)
         print(images[type].get_height())
-        if images[type].get_height() == 71:
+        if images[type].get_height() == SMALL_CACTUS_HEIGHT:
             self.rect.y = 325
 
-        if images[type].get_height() == 95:
+        if images[type].get_height() == LARGE_CACTUS_HEIGHT:
             self.rect.y = 301
             
